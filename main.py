e17608c365e88a3d6f1b64ebe7b2e5ab5024ab3f
BarracudaPff <mrm.kikll@gmail.com>
2023-11-16 21:58:08 +0100

    more

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ def run_in_parallel(tasks):
 if __name__ == '__main__':
     run_in_parallel([
         lambda: BruteForce("store-1/CAT-1.log").main(),
-        # lambda: BruteForce("store-1/CAT-2.log").main(),
-        # lambda: BruteForce("store-1/CAT-3.log").main(),
-        # lambda: BruteForce("store-1/CAT-4.log").main(),
-        # lambda: BruteForce("store-1/CAT-5.log").main(),
-        # lambda: BruteForce("store-1/CAT-6.log").main(),
-        # lambda: BruteForce("store-1/CAT-7.log").main(),
-        # lambda: BruteForce("store-1/CAT-8.log").main(),
-        # lambda: BruteForce("store-1/CAT-9.log").main(),
-        # lambda: BruteForce("store-1/CAT-10.log").main(),
+        lambda: BruteForce("store-1/CAT-2.log").main(),
+        lambda: BruteForce("store-1/CAT-3.log").main(),
+        lambda: BruteForce("store-1/CAT-4.log").main(),
+        lambda: BruteForce("store-1/CAT-5.log").main(),
+        lambda: BruteForce("store-1/CAT-6.log").main(),
+        lambda: BruteForce("store-1/CAT-7.log").main(),
+        lambda: BruteForce("store-1/CAT-8.log").main(),
+        lambda: BruteForce("store-1/CAT-9.log").main(),
+        lambda: BruteForce("store-1/CAT-10.log").main(),
     ])
